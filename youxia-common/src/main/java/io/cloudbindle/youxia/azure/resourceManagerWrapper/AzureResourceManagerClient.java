163fde35635ca285a676e89ee0224b70b04dba34
Denis Yuen <dyuen@oicr.on.ca>
2015-07-24 11:43:19 -0400

    Feedback

--- youxia-common/src/main/java/io/cloudbindle/youxia/azure/resourceManagerWrapper/AzureResourceManagerClient.java
+++ youxia-common/src/main/java/io/cloudbindle/youxia/azure/resourceManagerWrapper/AzureResourceManagerClient.java
@@ -22,6 +22,7 @@ import com.microsoft.aad.adal4j.AuthenticationContext;
 import com.microsoft.aad.adal4j.AuthenticationResult;
 import io.cloudbindle.youxia.util.ConfigTools;
 import static io.cloudbindle.youxia.util.ConfigTools.YOUXIA_AZURE_SUBSCRIPTION_ID;
+import io.cloudbindle.youxia.util.Log;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -49,6 +50,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 public class AzureResourceManagerClient {
 
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static final String AZURE_LOGIN_AUTHORITY = "login.windows.net";
+    public static final String AZURE_MANAGEMENT_DOMAIN = "management.azure.com";
+    public static final String AZURE_API_VERSION = "2015-01-01";
 
     private AuthenticationResult getAccessTokenFromUserCredentials() throws Exception {
         // this user is an "organization user" microsoft accounts do not work
@@ -58,7 +62,8 @@ public class AzureResourceManagerClient {
         String username = youxiaConfig.getString(ConfigTools.YOUXIA_AZURE_ACTIVE_DIRECTORY_USERNAME);
         String password = youxiaConfig.getString(ConfigTools.YOUXIA_AZURE_ACTIVE_DIRECTORY_PASSWORD);
         // the end part of this is from the URL of the active directory page, seriously
-        String authority = "https://login.windows.net/" + youxiaConfig.getString(ConfigTools.YOUXIA_AZURE_ACTIVE_DIRECTORY_TENANT_ID);
+        String authority = "https://" + AZURE_LOGIN_AUTHORITY + "/"
+                + youxiaConfig.getString(ConfigTools.YOUXIA_AZURE_ACTIVE_DIRECTORY_TENANT_ID);
         // this is listed under the applications tab and is listed as CLIENT ID
         String clientID = youxiaConfig.getString(ConfigTools.YOUXIA_AZURE_ACTIVE_DIRECTORY_CLIENT_ID);
 
@@ -68,7 +73,8 @@ public class AzureResourceManagerClient {
         try {
             service = Executors.newFixedThreadPool(1);
             context = new AuthenticationContext(authority, false, service);
-            Future<AuthenticationResult> future = context.acquireToken("https://management.azure.com/", clientID, username, password, null);
+            Future<AuthenticationResult> future = context.acquireToken("https://" + AZURE_MANAGEMENT_DOMAIN + "/", clientID, username,
+                    password, null);
             result = future.get();
         } finally {
             service.shutdown();
@@ -87,8 +93,8 @@ public class AzureResourceManagerClient {
         accessToken = accessTokenFromUserCredentials.getAccessToken();
 
         HttpClient client = new DefaultHttpClient();
-        HttpGet request = new HttpGet("https://management.azure.com/subscriptions/" + youxiaConfig.getString(YOUXIA_AZURE_SUBSCRIPTION_ID)
-                + "/" + endpoint + "?api-version=2015-01-01");
+        HttpGet request = new HttpGet("https://" + AZURE_MANAGEMENT_DOMAIN + "/subscriptions/"
+                + youxiaConfig.getString(YOUXIA_AZURE_SUBSCRIPTION_ID) + "/" + endpoint + "?api-version=" + AZURE_API_VERSION);
         request.addHeader("Authorization", "Bearer " + accessToken);
         HttpResponse response = client.execute(request);
         if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
@@ -115,8 +121,8 @@ public class AzureResourceManagerClient {
             throw new RuntimeException(ex);
         }
         HttpClient client = new DefaultHttpClient();
-        HttpPatch request = new HttpPatch("https://management.azure.com/subscriptions/"
-                + youxiaConfig.getString(YOUXIA_AZURE_SUBSCRIPTION_ID) + "/" + endpoint + "?api-version=2015-01-01");
+        HttpPatch request = new HttpPatch("https://" + AZURE_MANAGEMENT_DOMAIN + "/subscriptions/"
+                + youxiaConfig.getString(YOUXIA_AZURE_SUBSCRIPTION_ID) + "/" + endpoint + "?api-version=" + AZURE_API_VERSION);
         request.setHeader("Content-Type", ContentType.APPLICATION_JSON.toString());
         request.setEntity(new StringEntity(payload, ContentType.APPLICATION_JSON));
         request.addHeader("Authorization", "Bearer " + accessToken);
@@ -156,32 +162,33 @@ public class AzureResourceManagerClient {
     }
 
     public static void main(String[] args) {
+        String targetName = "iron-head-4bf54097-1561-445e-b494-ee1d0f7035d0";
         AzureResourceManagerClient client = new AzureResourceManagerClient();
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
-            System.out.println("List of resource groups");
+            Log.info("List of resource groups");
             ResourceGroupList resourceGroupList = client.getResourceGroupList();
             String json = gson.toJson(resourceGroupList);
-            System.out.println(json);
+            Log.info(json);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
         try {
-            System.out.println("Specific resource groups");
-            ResourceGroup fromJson = client.getResourceGroup("test-pc-launch");
+            Log.info("Specific resource groups");
+            ResourceGroup fromJson = client.getResourceGroup(targetName);
             String json = gson.toJson(fromJson);
-            System.out.println(json);
+            Log.info(json);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
         try {
-            System.out.println("Update specific resource group");
-            ResourceGroup initialGroup = client.getResourceGroup("test-pc-launch");
+            Log.info("Update specific resource group");
+            ResourceGroup initialGroup = client.getResourceGroup(targetName);
             Map<String, String> tags = initialGroup.getTags();
             tags.put(String.valueOf(System.currentTimeMillis()), "funky_new_tag");
-            ResourceGroup updateResourceGroup = client.patchResourceGroup("test-pc-launch", tags);
+            ResourceGroup updateResourceGroup = client.patchResourceGroup(targetName, tags);
             String json = gson.toJson(updateResourceGroup);
-            System.out.println(json);
+            Log.info(json);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
